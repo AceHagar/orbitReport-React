9317e07fd6a9972fdd52114da6f2f139c164d47f
AceHagar <andyhaag@gmail.com>
2023-08-11 09:28:41 -0500

    Finished with all tests passing

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,41 @@
+import styles from "./styling.css"
+
 const Table = ({ sat }) => {
   return (
-      <table>
-       <thead>
+    <table>
+      <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
-        </tbody>
-      </table>
+      </thead>
+      <tbody>
+        {sat.map((data, id) => {
+          let isActive;
+          if (data.operational === true) {
+            isActive = "Active"
+          }  else {
+            isActive = "Inactive"
+          }
+                
+          return (
+            <tr key={id}>
+              <td>{data.name}</td>
+              <td>{data.type}</td>
+              <td>{data.launchDate}</td>           
+              <td>{isActive}</td>
+            </tr>
+          )
+
+        })}
+
+      </tbody>
+    </table>
   );
 };
 
 export default Table;
+
+
